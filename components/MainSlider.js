cea88111adc072f84bbff878c4b7e66856ab6d23
wyu <watsonyu@gmail.com>
2021-04-19 12:09:01 +0800

    use react-multi-carousel

--- components/MainSlider.js
+++ components/MainSlider.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const imageOne = "/products/chair10.png";
 const imageTwo = "/products/chair9.png";
 const imageThree = "/products/chair8.png";
+const imageFour = "/products/chair7.png";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
 
 const MainSlider = () => (
-  <Carousel plugins={['arrows']}>
-    <img src={imageOne} />
-    <img src={imageTwo} />
-    <img src={imageThree} />
-  </Carousel>
+<Carousel
+  swipeable={true}
+  draggable={true}
+  showDots={true}
+  responsive={responsive}
+  ssr={true} // means to render carousel on server-side.
+  infinite={true}
+  autoPlay={true}
+  autoPlaySpeed={3000}
+  keyBoardControl={true}
+  customTransition="all .5"
+  transitionDuration={1000}
+  containerClass="carousel-container"
+  removeArrowOnDeviceType={["tablet", "mobile"]}
+  dotListClass="custom-dot-list-style"
+  itemClass="carousel-item-padding-40-px"
+>
+  <div><img src={imageOne} /></div>
+  <div><img src={imageTwo} /></div>
+  <div><img src={imageThree} /></div>
+  <div><img src={imageFour} /></div>
+</Carousel>
 );
 
 export default MainSlider;
